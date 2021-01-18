989e5b5b6d0df23005537ecc35228bc6c5ce2df9
yusupp11maulana <yusupp11maulana@gmail.com>
2021-01-18 18:12:39 +0700

    Update customer.php, index.php, and login.php

--- application/views/index.php
+++ application/views/index.php
@@ -1,132 +1,269 @@
-<!doctype html>
+<!--
+=========================================================
+Material Kit - v2.0.7
+=========================================================
+
+Product Page: https://www.creative-tim.com/product/material-kit
+Copyright 2020 Creative Tim (https://www.creative-tim.com/)
+
+Coded by Creative Tim
+
+=========================================================
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
-
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-  <link rel="stylesheet" href="<?= base_url()?>style.css">
-  <script type="text/javascript" src="assets/sweetalert/sweetalert.min.js"></script>
-
-  <!-- ICON WEB -->
-	<link rel="icon" type="image/png" href="./asset/img/indische/logo.png">
-
+  <meta charset="utf-8" />
+  <link rel="icon" type="image/png" href="./asset/img/indische/logo.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Indische1931 Cafe - Beranda
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="./asset/css/material-kit.css?v=2.0.7" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./asset/demo/demo.css" rel="stylesheet" />
 </head>
 
-<body>
-  <div class="row" style="margin-top: 8%;">
-    <div class="col-lg-6">
-      <img src="<?= base_url()?>assets/cafe/cafe.png" alt="" style="width: 60%; margin-left: 30%">
+<body class="index-page sidebar-collapse">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg " color-on-scroll="0" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="<?= base_url()?>customer">
+          Indische1931 Cafe</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url()?>customer">
+              <i class="material-icons">home</i> Beranda
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url()?>order">
+              <i class="material-icons">assignment</i> Order Now 
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url()?>history">
+              <i class="material-icons">history</i> History 
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/indische1931" target="_blank" data-original-title="Follow us on Instagram" rel="nofollow">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+          <li class="nav-item bg-warning" style="border-radius: 60px; ">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="" target="_blank" data-original-title="Log Out Account" rel="nofollow">Login</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="col-lg-5">
-      <div class="container">
-        <!-- ROW LOGIN -->
-        <div class="row">
-          <div class="col mt-4">
-            <div class="d-flex justify-content-center">
-              <ul class="nav" role="tablist">
-                <li class="nav-item">
-                  <a href="#step-1" id="step1-tab" class="nav-link active" aria-selected="true" data-toggle="tab" role="tab" style="margin-left: -25px">
-                    <h5>CUSTOMER</h5>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="#step-2" id="step2-tab" class="nav-link" aria-selected="false" data-toggle="tab" role="tab" style="margin-left: -10px">
-                    <h5>OFFICIAL</h5>
-                  </a>
-                </li>
-                <div class="panel rounded">
-                </div>
-              </ul>
-            </div>
-            <div class="tab-content">
-              <!-- MHS -->
-              <div class="tab-pane fade show active" id="step-1" area-labelledby="step1-tab" role="tab-panel">
-                <div class="innerContent">
-                  <div class="login-content justify-content-center">
-                    <form action="login-mhs.php" method="POST">
-                      <div class="input-div one">
-                        <div class="i">
-                          <i class="fas fa-user"></i>
-                        </div>
-                        <div class="div">
-                          <h5>Email</h5>
-                          <input type="text" name="email" class="input">
-                        </div>
-                      </div>
-                      <div class="input-div pass">
-                        <div class="i">
-                          <i class="fas fa-lock"></i>
-                        </div>
-                        <div class="div">
-                          <h5>Password</h5>
-                          <input type="password" name="telp_mhs" class="input">
-                        </div>
-                      </div>
-                      <input type="submit" class="btn2" value="Login">
-                    </form>
-                  </div>
-                </div>
-                <div class="container">
-                  <p class="ml-5 mt-4">Don't have an account?</p>
-                  <a href="<?= base_url()?>login/regist"><button type="button" class="btn btn-outline-dark">CREATE</button></a>
-                </div>
-              </div>
-              <!-- /MHS -->
-
-              <!-- ADM -->
-              <div class="tab-pane fade" id="step-2" area-labelledby="step2-tab" role="tab-panel">
-                <div class="innerContent">
-                  <div class="login-content justify-content-center">
-                    <form action="index.php?aksi=admin" method="POST">
-                      <div class="input-div one">
-                        <div class="i">
-                          <i class="fas fa-user"></i>
-                        </div>
-                        <div class="div">
-                          <h5>NIP</h5>
-                          <input type="text" class="input" id="unameadm" name="unameadm">
-                        </div>
-                      </div>
-                      <div class="input-div pass">
-                        <div class="i">
-                          <i class="fas fa-lock"></i>
-                        </div>
-                        <div class="div">
-                          <h5>Password</h5>
-                          <input type="password" class="input" id="passadm" name="passadm">
-                        </div>
-                      </div>
-                      <input type="submit" class="btn2" value="Login">
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <!-- /ADM -->
-            </div>
+  </nav>
+  <div class="page-header header-filter" data-parallax="true" style="background-image: url('./asset/img/indische/bg.png');">
+    <div class="container" style="margin-top: -100px">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand">
+            <h1>Indische1931 Cafe</h1>
+            <h3>Coffee Roastery HahaHihi</h3>
+            <button class="btn btn-success btn-round mt-3">
+              <i class="material-icons">assignment</i> Order Now
+            </button>
           </div>
         </div>
-        <!-- /ROW LOGIN -->
       </div>
     </div>
   </div>
+  <div class="main main-raised mb-5" style="margin-top: -200px">
+    <!--         carousel  -->
+    <div class="section" id="carousel">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mr-auto ml-auto">
+            <!-- Carousel Card -->
+            <div class="card card-raised card-carousel">
+              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="./asset/img/indische/carousel1.png" alt="First slide">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h4>
+                        <i class="material-icons">location_on</i>
+                        Indische1931 Cafe, Jl. Brigjend Katamso No.179 Sidoarjo
+                      </h4>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="./asset/img/indische/carousel2.png" alt="Second slide">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h4>
+                        <i class="material-icons">location_on</i>
+                        Indische1931 Cafe, Jl. Brigjend Katamso No.179 Sidoarjo
+                      </h4>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="./asset/img/indische/carousel3.png" alt="Third slide">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h4>
+                        <i class="material-icons">location_on</i>
+                        Indische1931 Cafe, Jl. Brigjend Katamso No.179 Sidoarjo
+                      </h4>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="./asset/img/indische/carousel4.png" alt="Fourth slide">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h4>
+                        <i class="material-icons">location_on</i>
+                        Indische1931 Cafe, Jl. Brigjend Katamso No.179 Sidoarjo
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <i class="material-icons">keyboard_arrow_left</i>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <i class="material-icons">keyboard_arrow_right</i>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+            <!-- End Carousel Card -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--         end carousel -->
+    <div class="section" style="margin-top: -100px">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-8 mx-auto text-center">
+            <h2>Indische1931 Cafe</h2>
+            <h4>Cafe Indische 1931 merupakan cafe yang berlokasi di Jl. Brigjend Katamso No.179, Kedungrejo, Kec. Waru, Kabupaten Sidoarjo, Jawa Timur 61256. Cafe ini memiliki tema yang vintage yang cocok bagi anak muda jaman sekarang.</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CARD SECTION -->
+    <div class="section">
+      <div class="row ml-5">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-header card-header-icon card-header-rose">
+              <div class="card-icon">
+                <div class="col text-center">
+                  <h4 class="card-title">Sejarah Cafe</h4>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="container">
+                <img class="d-block w-100" src="./asset/img/indische/history.png">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 ml-4">
+          <p class="h5" style="margin-top: 15%">Bangunan pada cafe ini terbangun pada tahun 1931 dan digunakan tempat berkumpulnya organisasi islam pada waktu itu. tempat ini terselamatkan dari serangan bom penjajah sebelum tahun 1945. 88 tahun sejak tempat ini di bangun tepatnya tahun 2019, cucu dari pemilik ini merenovasi bangunan ini menjadi sebuah cafe modern tanpa menghilangkan nuansa vintage untuk tempat berkumpulnya para pemuda di masa ini.</p>
+        </div>
+      </div>
+      <div class="row text-right">
+        <div class="col-md-3 ml-4"></div>
+        <div class="col-md-5 mr-2">
+          <p class="h5" style="margin-top: 15%">Indische1931 Cafe memiliki suasana yang mendukung untuk nongkrong bersama teman dan keluarga, selain itu indische1931 Cafe mengusung konsep cafe yang tidak biasa - biasa saja seperti cafe pada umumnya. cafe ini memiliki konsep cafe modern bertema vintage sehingga pengunjung dapat mengabadikan momen saat nongkrong dangan estetik. selain itu tempat di cafe ini memiliki tingkat kebersihan yang bagus sehingga dapat membuat pengunjung merasa nyaman. cafe ini memiliki lokasi yang strategis karena dekat dengan jalan utama di Jalan Raya Waru.</p>
+        </div>
+        <div class="col-md-3 ml-4">
+          <div class="card">
+            <div class="card-header card-header-icon card-header-rose">
+              <div class="card-icon">
+                <div class="col text-center">
+                  <h4 class="card-title">Suasana Cafe</h4>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="container">
+                <img class="d-block w-100" src="./asset/img/indische/aestetik.png">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /CARD SECTION -->
+    <!-- Footer -->
+    <div class="section section-download" id="downloadSection" style="margin-top: -150px">
+      <div class="container">
+        <div class="sharing-area text-center">
+          <div class="row justify-content-center">
+            <h3>Thank you for supporting us!</h3>
+          </div>
+          <a id="instagram" class="btn btn-raised btn-instagram" href="https://www.instagram.com/indische1931" rel="nofollow">
+            <i class="fa fa-instagram"></i>
+            Instagram
+          </a>
+          <a id="github" href="https://indischegroup.com/" target="_blank" class="btn btn-raised btn-github" rel="nofollow">
+            <i class="fa fa-chrome"></i>
+            Indischegroup
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+  </div>
+  <!--   Core JS Files   -->
+  <script src="./asset/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="./asset/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="./asset/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="./asset/js/plugins/moment.min.js"></script>
+  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="./asset/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="./asset/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+  <script src="./asset/js/material-kit.js?v=2.0.7" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      //init DateTimePickers
+      materialKit.initFormExtendedDatetimepickers();
 
-  
-  <!--  -->
-  <!-- Optional JavaScript -->
-  <!-- CDN JQuery -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  <script src="<?= base_url()?>main.js"></script>
+      // Sliders Init
+      materialKit.initSliders();
+    });
+
+
+    function scrollToDownload() {
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
 </body>
 
 </html>
